ce1deabb02138b291b5c023a4e6412b866e9b6c2
Crissss17 <cristian.moyano@alumnos.ucn.cl>
2024-09-26 21:22:02 -0300

    Docker Listo

--- backend/ms/dist/app.module.js
+++ backend/ms/dist/app.module.js
@@ -16,7 +16,7 @@ exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
-            mongoose_1.MongooseModule.forRoot('mongodb+srv://cristianmoyano:TheCriss_17@cluster0.v96or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+            mongoose_1.MongooseModule.forRoot('mongodb://localhost:27017'),
             user_module_1.UserModule
         ],
     })
--- backend/src/ms-iam/app.module.ts
+++ backend/src/ms-iam/app.module.ts
@@ -4,7 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://cristianmoyano:TheCriss_17@cluster0.v96or.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    MongooseModule.forRoot('mongodb://localhost:27017'),
     UserModule],
 })
 export class AppModule {}
